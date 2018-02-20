c4a48003823365d4f6ed123237350b497f108048
Changyao <changyao.chen@gmail.com>
2018-02-19 23:53:45 -0500

    working on hw3, make streaming data work

---
+++ rev_part_2/hw3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+The goal of this problem is to implement the "Median Maintenance" algorithm (covered in the Week 3 
+lecture on heap applications). The text file contains a list of the integers from 1 to 10000 in unsorted order; 
+you should treat this as a stream of numbers, arriving one by one. Letting xi denote the ith number of the file, 
+the kth median mk is defined as the median of the numbers x1,…,xk. (So, if k is odd, then mk is ((k+1)/2)th smallest 
+number among x1,…,xk; if k is even, then mk is the (k/2)th smallest number among x1,…,xk.)
+
+In the box below you should type the sum of these 10000 medians, modulo 10000 (i.e., only the last 4 digits). 
+That is, you should compute (m1+m2+m3+⋯+m10000)mod10000.
+"""
+
+class Solution(object):
+
+	def __init__(self):
+		return
+
+	def stream(self, fname):
+		
+		with open(fname, 'r') as f:
+			while True:
+				line = f.readline()
+				if not line:
+					break
+				yield line.strip()
+
+	def get_median(self, fname):
+		for line in self.stream(fname):
+			print(line)
+
+
+if __name__ == '__main__':
+
+	S = Solution()
+	S.get_median('hw3.txt')
